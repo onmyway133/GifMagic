6c66091181863762a211a188b2db9a57e723cae6
Khoa Pham <onmyway133@gmail.com>
2017-07-31 16:10:28 +0200

    Update Playground

--- Playground-Mac.playground/Contents.swift
+++ Playground-Mac.playground/Contents.swift
@@ -4,3 +4,18 @@ import Cocoa
 import GifMagic
 
 var str = "Hello, playground"
+let images: [NSImage] = Array(0..<12)
+  .map({
+    let url = URL(fileURLWithPath: NSHomeDirectory(), relativeTo: nil)
+      .appendingPathComponent("Downloads")
+      .appendingPathComponent("badge4-\($0)@2x")
+      .appendingPathExtension("png")
+
+    return url
+  })
+  .flatMap({
+    return NSImage(contentsOf: $0)
+  })
+
+let fileUrl = GifMagic.Encoder().encode(images: images, frameDuration: 0.2)
+print(fileUrl)
