145c5e5bec4cf0b20f991775e516fced3de98ab7
Khoa Pham <onmyway133@gmail.com>
2017-08-01 14:29:04 +0200

    Inject dependencies

--- Sources/Mac/Handler.swift
+++ Sources/Mac/Handler.swift
@@ -3,14 +3,22 @@ import Cocoa
 import CoreGraphics
 
 public class Handler {
+  public let encoder: Encoder
+  public let decoder: Decoder
+
+  public init(encoder: Encoder, decoder: Decoder) {
+    self.encoder = encoder
+    self.decoder = decoder
+  }
+
   /// Modify each frame in the gif
   public func modify(fileUrl: URL, closure: (NSImage) -> NSImage ) -> URL? {
-    guard let result = Decoder().decode(fileUrl: fileUrl) else {
+    guard let result = decoder.decode(fileUrl: fileUrl) else {
       return nil
     }
 
     let images = result.images.map(closure)
 
-    return Encoder().encode(images: images, frameDuration: result.gifInfo.frameDuration)
+    return encoder.encode(images: images, frameDuration: result.gifInfo.frameDuration)
   }
 }
